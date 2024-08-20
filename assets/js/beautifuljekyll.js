a05e69a1ec775df375316f9a93be33c6695788f7
Abel <habelor@gmail.com>
2024-08-20 14:48:56 +0200

    Update beautifuljekyll.js

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -141,14 +141,3 @@ let BeautifulJekyllJS = {
 
 document.addEventListener('DOMContentLoaded', BeautifulJekyllJS.init);
 
-$(function() {
-  $('#change-skin').on('click', function () {
-    $("body").toggleClass("page-dark-mode");
-    localStorage.setItem('bj-dark-mode', $("body").hasClass("page-dark-mode"));
-    BeautifulJekyllJS.initNavbar();
-  });
-  if (localStorage.getItem('bj-dark-mode') === 'true') {
-    $('#change-skin').trigger('click');
-  }
-});
-
